aef997fab3309305a5dc57afe2ef5b37e0e83bcd
avniagwl1 <avni.agwl1@gmail.com>
2022-08-20 11:36:36 -0700

    Initial Commit

---
+++ Shared/password_genoratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  password_genoratorApp.swift
+//  Shared
+//
+//  Created by Avni Agarwal on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct password_genoratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
